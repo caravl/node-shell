a2dde4cc72ec5d8446745528ccdab60c161e720a
Caryn McCarthy <cmccarthy15@gmail.com>
2017-09-18 14:45:06 -0400

    piping successful

--- bash.js
+++ bash.js
@@ -1,23 +1,28 @@
 // Output a prompt
 var command = require('./commands.js');
-
-var done = function(output){
-  //show the output
-  process.stdout.write(output);
-  //show the prompt
-  process.stdout.write('\nprompt > ');
-};
-
 process.stdout.write('prompt > ');
+
 
 //The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  // var cmds = data.toString().trim().split(' ');// remove the newline
-  // var cmd = cmds[0];
+  var commandNum = 0;
   var cmdString = data.toString().trim();
-  var cmdList = cmdString.split(/\s*\|\s*/g) // any amount of whitespace, pipe, any amount of whitespace
-  // process.stdout.write(cmdList.toString());
-  // process.stdout.write('prompt > ');
-  var firstCommand = cmdList[0].split(' ');
-  command[firstCommand[0]](null, firstCommand.slice(1), done);
+  var cmdList = cmdString.split(/\s*\|\s*/g); // any amount of whitespace, pipe, any amount of whitespace
+  var singleCommand = cmdList[commandNum].split(' ');
+
+
+  var done = function(output){
+    if (commandNum === cmdList.length - 1) {
+      process.stdout.write(output);
+      process.stdout.write('\nprompt > ');
+    } else {
+      commandNum++;
+      singleCommand = cmdList[commandNum].split(' ');
+      command[singleCommand[0]](output, singleCommand.slice(1), done);
+    }
+  };
+
+
+  command[singleCommand[0]](null, singleCommand.slice(1), done);
 });
+
--- commands.js
+++ commands.js
@@ -6,9 +6,11 @@ module.exports = {
   pwd: function(stdin, file, done) {
     done(process.argv[1]);
   },
+
   date: function(stdin, file, done) {
     done(Date().toString());
   },
+
   ls: function(stdin, file, done) {
     var output = '';
     fs.readdir('.', function(err, files) {
@@ -19,39 +21,68 @@ module.exports = {
       done(output);
   });
   },
+
   echo: function(stdin, args, done) {
-    done(args.join(' '));
+      var input = args ? args.join(' ') : stdin;
+      done(input);
   },
+
   cat: function(stdin, filename, done) {
-    fs.readFile(filename[0], function(err, files) {
-      if (err) throw err;
-      done(files);
-    });
+    if (filename){
+      fs.readFile(filename[0], function(err, files) {
+        if (err) throw err;
+        done(files);
+      });
+    } else {
+      done(stdin);
+    }
   },
+
   head: function(stdin, filename, done) {
-    fs.readFile(filename[0], function(err, files) {
-      if (err) throw err;
-      var temp = files.toString().split('\n');
+    if (filename){
+      fs.readFile(filename[0], function(err, files) {
+        if (err) throw err;
+        var temp = files.toString().split('\n');
+        var firstFive = temp.slice(0, 5).join('\n');
+        done(firstFive);
+      });
+    } else {
+      var temp = stdin.toString().split('\n');
       var firstFive = temp.slice(0, 5).join('\n');
       done(firstFive);
-    });
+    }
   },
+
   tail: function(stdin, filename, done) {
-    fs.readFile(filename[0], function(err, files) {
-      if (err) throw err;
-      var temp = files.toString().split('\n');
+    if (filename){
+      fs.readFile(filename[0], function(err, files) {
+        if (err) throw err;
+        var temp = files.toString().split('\n');
+        var lastFive = temp.slice(temp.length - 5, temp.length).join('\n');
+        done(lastFive);
+      });
+    } else {
+      var temp = stdin.toString().split('\n');
       var lastFive = temp.slice(temp.length - 5, temp.length).join('\n');
       done(lastFive);
-    });
+    }
   }, // skipped implementing sort and uniq
+
   wc: function(stdin, filename, done) {
-    fs.readFile(filename[0], function(err, files) {
-      if (err) throw err;
-      var temp = files.toString().split('\n');
+    if (filename){
+      fs.readFile(filename[0], function(err, files) {
+        if (err) throw err;
+        var temp = files.toString().split('\n');
+        var length = temp.length.toString();
+        done(length);
+      });
+    } else {
+      var temp = stdin.toString().split('\n');
       var length = temp.length.toString();
       done(length);
-    });
+    }
   },
+
   curl: function(stdin, urlNameArray, done){
     request(urlNameArray[0], function(err, response, body){
       if (err){ throw err; }
